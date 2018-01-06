90016ead5747e572adf494401bf931e2819e5b8d
Kunihiko MIYOSHI <miyoshik@gmail.com>
2018-01-06 15:47:30 +0900

    add image importer from rmagick. succeeded to inference

--- rubyext/onnx.cpp
+++ rubyext/onnx.cpp
@@ -35,10 +35,10 @@ static VALUE wrap_onnx_init(VALUE self, VALUE vfilename) {
 
 struct instantModel {
     instant::model* model;
-    std::vector<std::tuple<std::string, instant::dtype_t,
-                           std::vector<int> const&, mkldnn::memory::format>>
-      input_def;
-    std::vector<std::string> output_def;
+    int batch_size;
+    int channel_num;
+    int width;
+    int height;
 };
 
 static instantModel* getModel(VALUE self) {
@@ -61,10 +61,10 @@ static VALUE wrap_model_alloc(VALUE klass) {
 static VALUE wrap_model_init(VALUE self, VALUE vonnx, VALUE vbatch_size,
                              VALUE vchannel_num, VALUE vheight, VALUE vwidth) {
 
-    int batch_size = NUM2INT(vbatch_size);
-    int channel_num = NUM2INT(vchannel_num);
-    int height = NUM2INT(vheight);
-    int width = NUM2INT(vwidth);
+    int batch_size = getModel(self)->batch_size = NUM2INT(vbatch_size);
+    int channel_num = getModel(self)->channel_num = NUM2INT(vchannel_num);
+    int height = getModel(self)->height = NUM2INT(vheight);
+    int width = getModel(self)->width = NUM2INT(vwidth);
 
     std::vector<int> input_dims{batch_size, channel_num, height, width};
 
@@ -95,35 +95,71 @@ static VALUE wrap_onnx_makeModel(VALUE self, VALUE vbatch_size,
     return obj;
 }
 
-static VALUE wrap_model_inference(VALUE self, VALUE image) {
+static VALUE wrap_model_inference(VALUE self, VALUE images) {
     VALUE vconv1_1_in_name = rb_str_new2("140326425860192");
 
-    // std::vector<float> image_data(mat.channels() * mat.rows * mat.cols);
-    std::cout << "DEBUG test 1 " << std::endl;
+    auto image_num = NUM2INT(rb_funcall(images, rb_intern("length"), 0, NULL));
+
+    std::vector<float> image_data(getModel(self)->channel_num *
+                                  getModel(self)->width *
+                                  getModel(self)->height);
+    // RMagick の形式を変換する
+    for(int i; i < image_num; i++) {
+        VALUE image = rb_ary_entry(images, 0);
+        VALUE raw_values =
+          rb_funcall(image, rb_intern("export_pixels"), 0, NULL);
+        auto value_num =
+          NUM2INT(rb_funcall(raw_values, rb_intern("length"), 0, NULL));
+        for(int y = 0; y < getModel(self)->height; ++y) {
+            for(int x = 0; x < getModel(self)->width; ++x) {
+                for(int c = 0; c < getModel(self)->channel_num; c++) {
+                    image_data[c * (getModel(self)->width *
+                                    getModel(self)->height) +
+                               y * getModel(self)->width + x] =
+                      static_cast<float>(
+                        NUM2INT(rb_ary_entry(
+                          raw_values, getModel(self)->channel_num *
+                                          (x + y * getModel(self)->height) +
+                                        c)) /
+                        257);
+                }
+            }
+        }
+    }
+    // std::cout << "DEBUG test 1 " << std::endl;
     // Copy input image data to model's input array
     auto& input_array = getModel(self)->model->input("140326425860192");
-    std::cout << "DEBUG test 2 " << std::endl;
+    // std::cout << "DEBUG test 2 " << std::endl;
 
-    // std::copy(image_data.begin(), image_data.end(),
-    // instant::fbegin(input_array));
+    std::copy(image_data.begin(), image_data.end(),
+              instant::fbegin(input_array));
 
     // Run inference
     auto const& output_table = getModel(self)->model->run();
-    std::cout << "DEBUG test 3 " << std::endl;
+    // std::cout << "DEBUG test 3 " << std::endl;
 
     // TODO
     // Get output
     auto const& fc6_out_arr =
       instant::find_value(output_table, "140326200777976");
-    std::cout << "fc6_out: ";
-    for(int i = 0; i < 5; ++i) {
-        std::cout << instant::fat(fc6_out_arr, i) << " ";
-    }
-    std::cout << "...\n";
+    // std::cout << "fc6_out: ";
+    // for(int i = 0; i < 5; ++i) {
+    //     std::cout << instant::fat(fc6_out_arr, i) << " ";
+    // }
+    // std::cout << "...\n";
 
     auto const& softmax_out_arr =
       instant::find_value(output_table, "140326200803680");
-
+    // std::cout << "softmax_out: ";
+    // for(int i = 0; i < 5; ++i) {
+    //     std::cout << instant::fat(fc6_out_arr, i) << " ";
+    // }
+    // 配列に積んで返す
+    VALUE result_array = rb_ary_new();
+    for(int i = 0; i < instant::total_size(softmax_out_arr); ++i) {
+        rb_ary_push(result_array, DBL2NUM(instant::fat(softmax_out_arr, i)));
+    }
+    return result_array;
 }
 
 /**
--- rubyext/test.rb
+++ rubyext/test.rb
@@ -1,10 +1,35 @@
+require 'rmagick'
+include Magick
+
 require './instant'
+
+# ONNXファイルを読み込む
 onnx_obj = ONNX.new("../data/VGG16.onnx")
 
+# 推論の条件
 batch_size = 1;
 channel_num = 3;
 height = 224;
 width = 224;
 
-a = onnx_obj.make_model(batch_size, channel_num, height, width)
-a.inference("test")
+# 推論に用いるネットワークを構築する
+model = onnx_obj.make_model(batch_size, channel_num, height, width)
+
+# 推論対象の画像を読み込む
+image = Image.read("../data/Light_sussex_hen.jpg").first
+image = image.resize_to_fill(width, height)
+
+# p image.export_pixels.map { |pix| pix/257 }.each_slice(3).each_slice(image.columns).to_a
+imageset = [image]
+inference_result = model.inference(imageset)
+
+# カテゴリ定義を読み込む
+categories = File.read('../data/synset_words.txt').split("\n")
+
+# スコアでソートする
+sorted_result = inference_result.zip(categories).sort_by{|x| -x[0]}
+
+# 推論結果を表示する
+sorted_result[0,5].each do |result|
+  puts "#{result[1]} : #{result[0]}"
+end
